80b567a51bf737ffa294eae00aa018e908ab5c5a
NoanWasTaken <delatouchenoan@gmail.com>
2026-02-03 12:23:04 +0100

    #3 complete admin dashboard and crud

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
