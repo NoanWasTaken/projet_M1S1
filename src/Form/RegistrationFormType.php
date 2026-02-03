7ce3d23d0f7104d2ee608cffd1aa698e7fa6c7f2
NoanWasTaken <delatouchenoan@gmail.com>
2026-02-03 19:05:40 +0100

    fixed .env on ci

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,36 +22,30 @@ class RegistrationFormType extends AbstractType
             ->add('name', TextType::class, [
                 'label' => 'First Name',
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter your first name',
-                    ]),
-                    new Length([
-                        'min' => 2,
-                        'minMessage' => 'Your first name should be at least {{ limit }} characters',
-                        'max' => 255,
-                    ]),
+                    new NotBlank(message: 'Please enter your first name'),
+                    new Length(
+                        min: 2,
+                        minMessage: 'Your first name should be at least {{ limit }} characters',
+                        max: 255,
+                    ),
                 ],
             ])
             ->add('surname', TextType::class, [
                 'label' => 'Last Name',
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter your last name',
-                    ]),
-                    new Length([
-                        'min' => 2,
-                        'minMessage' => 'Your last name should be at least {{ limit }} characters',
-                        'max' => 255,
-                    ]),
+                    new NotBlank(message: 'Please enter your last name'),
+                    new Length(
+                        min: 2,
+                        minMessage: 'Your last name should be at least {{ limit }} characters',
+                        max: 255,
+                    ),
                 ],
             ])
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new IsTrue(message: 'You should agree to our terms.'),
                 ],
             ])
             ->add('plainPassword', RepeatedType::class, [
@@ -67,14 +61,12 @@ class RegistrationFormType extends AbstractType
                 ],
                 'invalid_message' => 'The password fields must match.',
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        'max' => 4096,
-                    ]),
+                    new NotBlank(message: 'Please enter a password'),
+                    new Length(
+                        min: 6,
+                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        max: 4096,
+                    ),
                 ],
             ])
         ;
--- tests/Integration/Controller/HomeControllerTest.php
+++ tests/Integration/Controller/HomeControllerTest.php
@@ -12,6 +12,6 @@ class HomeControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Bienvenue'); 
+        $this->assertSelectorTextContains('h1', 'Votre équipement'); 
     }
 }
