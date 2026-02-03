c0920f3c768d42a0967a83d2f264b4ba2656ebb2
NoanWasTaken <delatouchenoan@gmail.com>
2026-02-03 16:22:48 +0100

    fixed .env on ci

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class ProductController extends AbstractController
+class ProductsController extends AbstractController
 {
     public function __construct(
         private ProductsRepository $productRepository
