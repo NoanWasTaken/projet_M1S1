0220e9bf279e01db57be8a336cb704c08e153308
Yannick <yk.105m@gmail.com>
2026-02-28 17:55:14 +0100

    page profil ajouté

---
+++ src/Controller/ClientProfileController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PlayerProfileRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class ClientProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_client_profile', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function index(PlayerProfileRepository $profileRepo): Response
+    {
+        $user = $this->getUser();
+        $profile = $profileRepo->findOneBy(['owner' => $user]); 
+
+        $xpTotal = $profile?->getXpTotal() ?? 0;
+        $level   = $profile?->getLevel() ?? 1;
+
+        $xpInLevel = $xpTotal % 1000;
+        $xpPercent = (int) round(($xpInLevel / 1000) * 100);
+
+        return $this->render('profile/profileClient.html.twig', [
+            'profile'   => $profile,
+            'level'     => $level,
+            'xpTotal'   => $xpTotal,
+            'xpInLevel' => $xpInLevel,
+            'xpPercent' => $xpPercent,
+        ]);
+    }
+}
